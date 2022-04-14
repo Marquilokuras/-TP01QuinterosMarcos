7cf60bdf5a667df268fc5966286ec427cece2ee0
Marcos Quinteros <92806059+Marquilokuras@users.noreply.github.com>
2022-04-14 15:28:29 -0300

    TPN°01QuinterosMarcos

---
+++ src/main/java/ar/edu/unju/edm/Tpn01QuinterosMarcosApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpn01QuinterosMarcosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tpn01QuinterosMarcosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ar/edu/unju/edm/model/package-info.java
@@ -0,0 +1,1 @@
+package ar.edu.unju.edm.model;
